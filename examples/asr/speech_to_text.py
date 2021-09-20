219c826a2df5056e29677bc1c3349cd6450dfec6
Vishaal MK <vishaalmk@gmail.com>
2021-09-20 08:49:37 +0530

    Testing code added

--- examples/asr/speech_to_text.py
+++ examples/asr/speech_to_text.py
@@ -66,7 +66,9 @@ For documentation on existing pretrained models, please visit -
 https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/asr/results.html
 
 """
-
+import os
+import jiwer
+import gspread
 import pytorch_lightning as pl
 from omegaconf import OmegaConf
 
@@ -76,12 +78,60 @@ from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 
 
+def eval(doc1, doc2):
+  transform = jiwer.Compose([
+      jiwer.RemovePunctuation(),
+      jiwer.RemoveMultipleSpaces(),
+      jiwer.ToLowerCase()
+  ])
+
+  hypothesis = transform(doc1)
+  ground_truth = transform(doc2)
+
+  measures = jiwer.compute_measures(ground_truth, hypothesis)
+
+#   print('Evaluation:')
+  scores = {
+      'Substitutions': measures['substitutions'],
+      'Deletions': measures['deletions'],
+      'Insertions': measures['insertions'],
+      'Total Error Rate': measures['wer'],
+  }
+#   print(data)
+#   print('----------------------------------------')
+
+  return scores
+
+def test(checkpoint_path):
+    asr_model = EncDecCTCModel.restore_from(checkpoint_path)
+    test_files = [file.split('.')[0].split('/')[-1] for file in os.listdir('test_files') if file.split('.')[-1] == 'wav']
+    for file in test_files:
+        asr_out = asr_model.transcribe(f"test_files/{file}.wav")
+        truth = open(f"test_files/{file}.txt").read()
+
+        return eval(asr_out, truth)
+        
+def update_sheet(score, checkpoint_path, name):
+    gc = gspread.service_account(filename='desicrew-v1-088082cf46f3.json')
+    sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1-OUI0RkDLE0OfOVS0a1PqBTNfGP0fjTMHkJWMUnoj9U/edit#gid=0")
+    sheet = sh.worksheet('Sheet2')
+
+    data = sheet.get_all_values()[1:]
+    index = len(data)
+
+
+    sheet.update_cell(index+2, 1, name)
+    sheet.update_cell(index+2, 2, checkpoint_path)
+    sheet.update_cell(index+2, 3, score['Total Error Rate'])
+    print('Sheet updated!')
+
+
 @hydra_runner(config_path="conf", config_name="config")
 def main(cfg):
     logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
 
     trainer = pl.Trainer(**cfg.trainer)
-    exp_manager(trainer, cfg.get("exp_manager", None))
+    log_directory = exp_manager(trainer, cfg.get("exp_manager", None))
     asr_model = EncDecCTCModel(cfg=cfg.model, trainer=trainer)
 
     # Initialize the weights of the model from another model, if provided via config
@@ -99,6 +149,12 @@ def main(cfg):
         )
         if asr_model.prepare_test(test_trainer):
             test_trainer.test(asr_model)
+    
+    checkpoints = os.listdir(log_directory + 'checkpoints')
+
+    for checkpoint_path in checkpoints:
+        update_sheet(test(checkpoint_path), checkpoint_path, log_directory)
+        
 
 
 if __name__ == '__main__':
